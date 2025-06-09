d40203618594becba4ad8971f98075c8a84ca2ed
General090 <gospelkalu33@gmail.com>
2025-06-09 11:36:37 +0100

    updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,12 +4,12 @@ export default function Home() {
   return (
     <div style={{ textAlign: "center", marginTop: 50 }}>
       <h1>Welcome to My App</h1>
-      <div style={{ marginTop: 20 }}>
+      <div style={{ marginTop: 20 }} className="flex gap-5">
         <Link to="/login">
-          <button style={{ marginRight: 10 }}>Login</button>
+          <button style={{ marginRight: 10 }} className="bg-blue-500">Login</button>
         </Link>
         <Link to="/register">
-          <button>Register</button>
+          <button className="bg-green-500">Register</button>
         </Link>
       </div>
     </div>
