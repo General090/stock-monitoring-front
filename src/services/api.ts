294be080e55c96d9b555efddfb333aab8cece62c
General090 <gospelkalu33@gmail.com>
2025-06-11 18:03:31 +0100

    updated

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://stock-monitoring-front.onrender.com",
+  baseURL: "https://stock-monitoring-back.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
